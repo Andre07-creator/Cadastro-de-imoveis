431c74a1bad48f16399dfcd38a39460cb6008a70
Andre07-creator <aramaldes876@gmail.com>
2023-02-14 21:38:27 -0300

    start

---
+++ index.js
@@ -0,0 +1,48 @@
+let choice = 0;
+let imobiles = [];
+
+do {
+  choice = prompt(
+    `Quantidade de imoveis: ${imobiles.length}\n (1) Salvar um imovel\n (2)Mostrar todos os imoveis salvos\n (3) Sair`
+  );
+
+  let showAll = "";
+  switch (choice) {
+    case "1":
+      let imobile = {};
+      imobile.name = prompt("Nome do proprietario:");
+      imobile.roomQuantity = prompt("Quantidade de quartos:");
+      imobile.bathroomQuantity = prompt("Quantidade de banheiros:");
+      imobile.garage = prompt("Possui garagem?");
+      imobiles.push(imobile);
+      console.log(imobile[0]);
+      break;
+    case "2":
+      for (let i = 0; imobiles.length > i; i++) {
+        let k = i + 1;
+        showAll +=
+          "Informação do imovel " +
+          k +
+          "\n" +
+          "Nome do proprietario: " +
+          imobiles[i].name +
+          "\n" +
+          "Numero de quartos: " +
+          imobiles[i].roomQuantity +
+          "\n" +
+          "Quantidade de banheiros: " +
+          imobiles[i].bathroomQuantity +
+          "\n" +
+          "Tem garagem: " +
+          imobiles[i].garage +
+          "\n\n";
+      }
+      alert(showAll);
+      break;
+    case "3":
+      alert("ecerrando!!!");
+      break;
+    default:
+      alert("Digite uma opção valida");
+  }
+} while (choice != "3");
